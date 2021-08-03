d347268b4d4c522fa2e9e9138c9a89e2cc97c7ca
Mauro Del Rio <mauro@crs4.it>
2021-08-03 14:43:19 +0200

    add tests for Composition.set

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pytest
+
+import openehr_client.data_types as data_types
 from openehr_client.flat import Composition, WebTemplateNode
 
 
@@ -7,12 +10,59 @@ def test_web_template_node(web_template_json):
     WebTemplateNode.create(web_template_json)
 
 
-def test_composition(web_template_json):
+@pytest.mark.skip("Composition.get TBD")
+def test_composition_set_path(web_template_json):
     web_template = WebTemplateNode.create(web_template_json)
     composition = Composition(web_template)
-    event0 = composition.root.add_descendant(
-        'molecular_markers/result_group/oncogenic_mutations_test/any_event')
-    event0.add_descendant('braf_pic3ca_her2_mutation_status').value = 1
-    composition.root.add_descendant(
-        'molecular_markers/result_group/oncogenic_mutations_test/any_event/braf_pic3ca_her2_mutation_status'
-    ).value = 2
+    text = 'ok'
+    path = '/test/context/status'
+    composition.set(path, text)
+    node = composition.get(path)
+    assert isinstance(node.value, data_types.Text)
+
+
+def test_composition_set_path_dv_text(web_template_json):
+    web_template = WebTemplateNode.create(web_template_json)
+    composition = Composition(web_template)
+    text = 'ok'
+    node = composition.set('/test/context/status', text)
+    assert isinstance(node.value, data_types.Text)
+    assert node.value.value == text
+
+
+def test_composition_set_path_code_phrase(web_template_json):
+    web_template = WebTemplateNode.create(web_template_json)
+    composition = Composition(web_template)
+    terminology = 'ISO_639-1'
+    code = 'en'
+    node = composition.set('/test/language',
+                           terminology=terminology,
+                           code=code)
+    assert isinstance(node.value, data_types.CodePhrase)
+    assert node.value.terminology == terminology
+    assert node.value.code == code
+
+
+def test_composition_set_path_dv_coded_text(web_template_json):
+    web_template = WebTemplateNode.create(web_template_json)
+    composition = Composition(web_template)
+    text = 'ok'
+    terminology = 'ISO_639-1'
+    code = 'en'
+    node = composition.set('/test/context/setting',
+                           text,
+                           terminology=terminology,
+                           code=code)
+    assert isinstance(node.value, data_types.CodedText)
+    assert node.value.value == text
+    assert node.value.terminology == terminology
+    assert node.value.code == code
+
+
+def test_composition_set_path_dv_datetime(web_template_json):
+    web_template = WebTemplateNode.create(web_template_json)
+    composition = Composition(web_template)
+    text = '2021-04-22T10:19:49.915Z'
+    node = composition.set('/test/context/start_time', text)
+    assert isinstance(node.value, data_types.DateTime)
+    assert node.value.value == text
