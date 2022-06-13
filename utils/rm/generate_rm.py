c2dc2b9e5367ac4413c61ac12a4201915402825d
Mauro Del Rio <mauro@crs4.it>
2022-06-13 19:20:18 +0200

    WIP, add rm creation

---
+++ utils/rm/generate_rm.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+from pathlib import Path
+
+from datamodel_code_generator import InputFileType, generate
+
+filename = "./openehr_rm_1.1.0_all.json"
+schema = open(filename, "r").read()
+
+
+def rename_class(name: str):
+    splitted = name.split("_")
+    return "".join([n.capitalize() if n != "DV" else n for n in splitted])
+
+
+template_dir = Path(os.path.dirname(os.path.realpath(__file__))) / Path("templates")
+output = Path("rm.py")
+generate(
+    schema,
+    input_file_type=InputFileType.JsonSchema,
+    output=output,
+    custom_template_dir=template_dir,
+    custom_class_name_generator=rename_class,
+    base_class=" ",
+)
+
+subprocess.check_call(
+    ["sed", "-i", "7s/^/from pydantic.dataclasses import dataclass\\n/", "rm.py"]
+)
+subprocess.check_call(["sed", "-i", "s/min_items=/minItems=/g", "rm.py"])
