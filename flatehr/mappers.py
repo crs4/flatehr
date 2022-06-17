2977445f4f0aee907be048fe59f7da8dbbbbf0bb
Mauro Del Rio <mauro@crs4.it>
2022-06-17 17:15:31 +0200

    add  converter

--- flatehr/mappers.py
+++ flatehr/mappers.py
@@ -1,6 +1,6 @@
 import abc
 from itertools import repeat
-from typing import IO, Dict, Iterable, Iterator, NewType, Optional, Tuple, Union
+from typing import IO, Any, Dict, Iterable, Iterator, NewType, Optional, Tuple, Union
 
 from lxml import etree
 from pipe import chain, map, sort
@@ -8,21 +8,22 @@ from pipe import chain, map, sort
 from flatehr.converters import Converter
 from flatehr.data_types import DATA_VALUE
 
-Path = NewType("Path", str)
+SourcePath = NewType("SourcePath", str)
+DestPath = NewType("DestPath", str)
 
 
 class Mapping(abc.ABC):
     @abc.abstractmethod
     def get_values(
         self, input_: Union[IO, str]
-    ) -> Iterator[Tuple[Path, Optional[DATA_VALUE]]]:
+    ) -> Iterator[Tuple[DestPath, Optional[DATA_VALUE]]]:
         ...
 
 
 class XPathMapping(Mapping):
     def __init__(
         self,
-        mapping: Dict[str, Path],
+        mapping: Dict[SourcePath, DestPath],
         converter: Optional[Converter] = None,
     ) -> None:
         self._mapping = mapping
@@ -30,7 +31,7 @@ class XPathMapping(Mapping):
 
     def get_values(
         self, input_: Union[IO, str]
-    ) -> Iterator[Tuple[Path, Optional[DATA_VALUE]]]:
+    ) -> Iterator[Tuple[DestPath, Optional[DATA_VALUE]]]:
         tree = etree.parse(input_)
         ns = tree.getroot().nsmap
         elements = (
@@ -46,7 +47,22 @@ class XPathMapping(Mapping):
             )
             | chain
             | sort(lambda el: el[2].sourceline)
-            | map(lambda el: (el[1], None if len(el[2].getchildren()) else el[2].text))
+            | map(
+                lambda el: (
+                    el[1],
+                    None
+                    if len(el[2].getchildren())
+                    else el[2].text
+                    if not self._converter
+                    else self._converter.convert(el[1], el[2].text),
+                )
+            )
         )
         for el in elements:
             yield el
+
+
+class Converter(abc.ABC):
+    @abc.abstractmethod
+    def convert(self, source_path: SourcePath, value) -> Any:
+        ...
