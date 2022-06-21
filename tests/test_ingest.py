df88d2adeebc2a46a28de67f5f13448ec88e93a2
Mauro Del Rio <mauro@crs4.it>
2022-06-21 15:20:13 +0200

    fix test ingest

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,7 +1,9 @@
 import pytest
-from flatehr.ingest import BasicIngester, MultiThreadedIngester
+from flatehr import template_factory
 
 
+@pytest.mark.parametrize("backend", template_factory.backends())
+@pytest.mark.parametrize("web_template_path", ["./tests/resources/web_template.json"])
 @pytest.mark.parametrize("n_compositions", [2])
 def test_ingester(ingester, ehr_composition_mapping, n_compositions):
     ehr_composition_mapping = ehr_composition_mapping(n_compositions)
