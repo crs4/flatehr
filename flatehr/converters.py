8bd41032cd3bdee9764a5bfb94b920528ff9486b
Mauro Del Rio <mauro@crs4.it>
2022-06-21 15:39:33 +0200

    add missing file

---
+++ flatehr/converters.py
@@ -0,0 +1,71 @@
+import abc
+from typing import Dict
+
+from flatehr.template import TemplateNode
+
+
+class Converter(abc.ABC):
+    def convert(self, template_node: TemplateNode, value: str) -> str:
+        ...
+
+
+class ValueConverter(Converter):
+    def __init__(self, mappings: Dict[str, str], passthrough: bool = False):
+        self._mappings = mappings
+        self._passthrough = passthrough
+
+    def convert(self, template_node: TemplateNode, value: str) -> str:
+        try:
+            return self._mappings[template_node._id]
+        except KeyError as ex:
+            if self._passthrough:
+                return value
+            raise ConversionFailed(f"{type(self)} failed with value {value}") from ex
+
+
+class DashValueConverter(Converter):
+    def __init__(self, *node_ids: str):
+        self._node_ids = node_ids
+
+    def convert(self, template_node: TemplateNode, value: str) -> str:
+        if template_node._id in self._node_ids:
+            try:
+                return value.split("-", 1)[1].strip()
+            except IndexError:
+                return value
+        raise ConversionFailed(f"{type(self)} cannot map value {value}")
+
+
+class InputBasedConverter(Converter):
+    def convert(self, template_node: TemplateNode, value: str) -> str:
+        if not template_node.inputs or "list" not in template_node.inputs[0]:
+            raise ConversionFailed(f"{type(self)} cannot map value {value}")
+        value_from_inputs = None
+        value_list = template_node.inputs[0]["list"]
+        for item in value_list:
+            label = item["label"].lower()
+            value_lower = value.lower()
+            if label == value_lower:
+                value_from_inputs = item["value"]
+                break
+        if value_from_inputs is None:
+            raise RuntimeError(f"invalid value {value} for node {template_node}")
+        return value_from_inputs
+
+
+class MultiConverter(Converter):
+    def __init__(self, *mappers: Converter):
+        self._mappers = mappers
+
+    def convert(self, template_node: TemplateNode, value: str) -> str:
+        for mapper in self._mappers:
+            try:
+                value = mapper.convert(template_node, value)
+                #  break
+            except ConversionFailed:
+                ...
+        return value
+
+
+class ConversionFailed(Exception):
+    ...
