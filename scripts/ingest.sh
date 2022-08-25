d6047ed9b73da289d52e3cbaaed9a5d06b4ebaf3
Mauro Del Rio <mauro@crs4.it>
2022-08-25 10:01:15 +0200

    update ingest.sh

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -8,9 +8,10 @@ parser.add_argument('-s', '--server', type=str, help='openehr base url', default
 parser.add_argument('-l', '--login', type=str, help='user:password', default='ehrbase-user:SuperSecretPassword')
 parser.add_argument('-t', '--template', type=str, help='web template', required=True)
 parser.add_argument('-c', '--conf', type=str, help='composition generation conf', required=True)
+parser.add_argument('-p', '--processes', type=int, help='Parallel processes used', default=1)
 EOF
 # set -xeu
-ls $INFILE  | xargs -n1 -P8   flatehr generate from-xml -c $CONF -t $TEMPLATE | \
+ls $INFILE  | xargs -n1 -P $PROCESSES   flatehr generate from-file -c $CONF -t $TEMPLATE | \
   tqdm --total $(ls $INFILE | wc -l)  | \
   while read ehr_id  comp ; do curl -X POST  -u $LOGIN -H 'Content-type: application/json' "$server/ehrbase/rest/ecis/v1/composition/?format=FLAT&ehrId=$ehr_id&templateId=crc_cohort_rev"  -d "$comp" 1>>out 2>&1 ; done 1>>out 2>&1
 
