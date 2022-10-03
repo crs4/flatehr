be1e02437bed23a2120741c89a387552d6c134a8
Mauro Del Rio <mauro@crs4.it>
2022-10-03 19:51:14 +0200

    fix: ingest script, server and template id variable

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 source $(dirname $0)/argparse.bash || exit 1
 argparse "$@" <<EOF || exit 1
 parser.add_argument('infile')
@@ -11,7 +10,8 @@ parser.add_argument('-c', '--conf', type=str, help='composition generation conf'
 parser.add_argument('-p', '--processes', type=int, help='Parallel processes used', default=1)
 EOF
 # set -xeu
+template_id=$(cat $TEMPLATE | jq -r .templateId)
 ls $INFILE  | xargs -n1 -P $PROCESSES   flatehr generate from-file -c $CONF -t $TEMPLATE | \
   tqdm --total $(ls $INFILE | wc -l)  | \
-  while read ehr_id  comp ; do curl -X POST  -u $LOGIN -H 'Content-type: application/json' "$server/ehrbase/rest/ecis/v1/composition/?format=FLAT&ehrId=$ehr_id&templateId=crc_cohort_rev"  -d "$comp" 1>>out 2>&1 ; done 1>>out 2>&1
+  while read ehr_id  comp ; do curl -X POST  -u $LOGIN -H 'Content-type: application/json' "$SERVER/ehrbase/rest/ecis/v1/composition/?format=FLAT&ehrId=92edfe4a-89ed-41ae-8485-de96f928664b&templateId=$template_id"  -d "$comp" ; done
 
