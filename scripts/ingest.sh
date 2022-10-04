d5a1aa5ef43b95b553dbf9c578aed5c0f88201c5
Mauro Del Rio <mauro@crs4.it>
2022-10-04 15:10:45 +0200

    feat: improve ingest script so that it create ehr based on ext_id

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -9,9 +9,23 @@ parser.add_argument('-t', '--template', type=str, help='web template', required=
 parser.add_argument('-c', '--conf', type=str, help='composition generation conf', required=True)
 parser.add_argument('-p', '--processes', type=int, help='Parallel processes used', default=1)
 EOF
-# set -xeu
+# set -xeuo
+
+echoerr() { printf "%s\n" "$*" >&2; }
+
 template_id=$(cat $TEMPLATE | jq -r .templateId)
+
+mkdir -p compositions
 ls $INFILE  | xargs -n1 -P $PROCESSES   flatehr generate from-file -c $CONF -t $TEMPLATE | \
   tqdm --total $(ls $INFILE | wc -l)  | \
-  while read ehr_id  comp ; do curl -X POST  -u $LOGIN -H 'Content-type: application/json' "$SERVER/ehrbase/rest/ecis/v1/composition/?format=FLAT&ehrId=92edfe4a-89ed-41ae-8485-de96f928664b&templateId=$template_id"  -d "$comp" ; done
+  while read ext_id  comp ; do    
+    # echoerr $ext_id
+    ehr_payload="{ \"_type\": \"EHR_STATUS\", \"archetype_node_id\": \"openEHR-EHR-EHR_STATUS.generic.v1\", \"name\": { \"value\": \"EHR Status\" }, \"subject\": { \"external_ref\": { \"id\": { \"_type\": \"GENERIC_ID\", \"value\": \"$ext_id\", \"scheme\": \"id_scheme\" }, \"namespace\": \"examples\", \"type\": \"PERSON\" } }, \"is_modifiable\": true, \"is_queryable\": true }"
+    ehr_id=$(curl -s -X POST -d "$ehr_payload"  -u ehrbase-user:SuperSecretPassword -H 'Prefer: return=representation' -H 'Content-type: application/json '  $SERVER/ehrbase/rest/openehr/v1/ehr  | jq -r .ehr_id.value)
+    echo $comp > compositions/${ehr_id}.json
+    # set -xeuo
+    curl -s -X POST  -u $LOGIN -H 'Content-type: application/json' "$SERVER/ehrbase/rest/ecis/v1/composition/?format=FLAT&ehrId=$ehr_id&templateId=$template_id"  -d "$comp" &>>  out  ;
+
+
+  done
 
